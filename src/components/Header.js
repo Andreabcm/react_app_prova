15e646147e2b840871f34e06c481c731f2d5b0d3
Andrea Cevallos <andreabcm16@gmail.com>
2022-10-07 15:06:47 +0200

    components de classe: creació

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import logo from '../img/qx7jYSYm_400x400 (1).jpg';
+
+class Header extends Component{
+
+    render(){
+        return(
+            <h1><img src={ logo } height="50px"/> El teu portal de la montanya. </h1>
+        )
+    }
+}
+
+export default Header
