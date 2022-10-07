ac3d413e6dc3e4f949188f5caf074e9e22687c05
Andrea Cevallos <andreabcm16@gmail.com>
2022-10-07 14:43:37 +0200

    JSX exemple inline styles

--- src/App.js
+++ src/App.js
@@ -6,17 +6,23 @@ function sumar(){
 
 const nom = 'Alex'
 
+const inLineStyle = {
+  color: "#2ecc71",
+  fotnSize: "26px",
+}
+
 function App() {
   return (
     <>
-      <div className="red">
-        <h2>Children</h2>
-      </div>
-      <h1 className="red"> Funcions { sumar() } </h1>
-      <h1> Operacions matemàtiques { 3+2 } </h1>
-      <h1> String { `Hola ${ nom }` }</h1>
-      <h1> Lògica / Ternary { (3+2) >6 ? 'mayor' : 'menor' } </h1>
-      <img src="" alt="" />
+      <p style={ inLineStyle }> inLineStyle </p>
+        <div className="red">
+          <h2>Children</h2>
+        </div>
+        <h1 className="red"> Funcions { sumar() } </h1>
+        <h1> Operacions matemàtiques { 3+2 } </h1>
+        <h1> String { `Hola ${ nom }` }</h1>
+        <h1> Lògica / Ternary { (3+2) >6 ? 'mayor' : 'menor' } </h1>
+        <img src="" alt="" />
     </>
   );
 }
