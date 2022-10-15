ac46844cdfc1040023dd998ae1f363a7d42a779d
Andrea Cevallos <andreabcm16@gmail.com>
2022-10-15 09:04:49 +0200

    important arxiu d'estils locals, al component Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Header.css';
 import logo from '../img/qx7jYSYm_400x400 (1).jpg';
 import Salutacio from './Salutacio.js';
 
@@ -7,9 +8,9 @@ class Header extends Component{
     render(){
         var usuari = "Andrea";
         return(
-            <h1><img src={logo} height="50px"/> El teu portal de la montanya. <Salutacio nick={usuari}/></h1>
+            <h1 className="salutacio"><img src={logo} height="50px" alt=""/> El teu portal de la montanya. <Salutacio nick={usuari}/></h1>
         )
     }
 }
 
-export default Header
+export default Header;
