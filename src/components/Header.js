181a95af40ca13ed6b6c67e8d0acce1c0a5c6ac8
Andrea Cevallos <andreabcm16@gmail.com>
2022-10-15 08:34:40 +0200

    component funcionals: implementació

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from '../img/qx7jYSYm_400x400 (1).jpg';
+import Salutacio from './Salutacio.js';
 
 class Header extends Component{
 
     render(){
+        var usuari = "Andrea";
         return(
-            <h1><img src={ logo } height="50px"/> El teu portal de la montanya. </h1>
+            <h1><img src={logo} height="50px"/> El teu portal de la montanya. <Salutacio nick={usuari}/></h1>
         )
     }
 }
