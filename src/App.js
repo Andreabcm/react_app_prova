4b71b97d0eec78aecd39770b4433f8b368bbf8e6
Andrea Cevallos <andreabcm16@gmail.com>
2022-10-15 14:52:12 +0200

    passar props a component funcional

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import './App.css';
 import Header from './components/Header.js';
+import functionalComponent from './components/FunctionalComponent';
 
 
 function App() {
-  
+  const propFunc1 = "variable";
+  const propsFuncObject = {
+    nom: "Alex",
+    edat: 25
+  } 
 
   return (
     <>
     <div className="bg-primary card">
       <Header/>
     </div>  
+    <div>
+      <functionalComponent 
+        propsFunctional1 = {propFunc1}
+        propsFunctional2String = "cadenaDeTexte1"
+        propsFunctional3Number = {3}
+        propFunctional4Object = {propsFuncObject}
+      />
+    </div>
     </>
   );
 }
